ed710a174fa338e9264b807be36e14c255e34da6
Matthias Hermann <matthias.herm@mailbox.org>
2020-09-09 11:59:14 +0200

    Extract methods for single processing steps

--- acg/card_data_generator.py
+++ acg/card_data_generator.py
@@ -10,8 +10,6 @@ from data.card import Card
 from template_processor import TemplateProcessor as tp
 from typing import List
 
-RELEVANT_SECTIONS = tuple(['Adjective', 'Adverb', 'Conjunction', 'Determiner', 'Interjection', 'Noun', 'Number',
-                    'Numeral', 'Ordinal number', 'Particle', 'Postposition', 'Preposition', 'Pronoun', 'Verb'])
 IRRELEVANT_SECTIONS = tuple(['Abbreviations', 'Alternative forms', 'Anagrams', 'Antonyms', 'Compounds', 'Conjugation',
                         'Contraction', 'Coordinate terms', 'Declension', 'Derived terms', 'Descendants',
                         'External links', 'Further reading', 'Hypernyms', 'Hyponyms', 'Idiom', 'Idioms', 'Inflection',
@@ -67,34 +65,82 @@ class CardDataGenerator(xml.sax.ContentHandler):
             self.in_text = False
 
     def _process(self):
+        """
+        Process a Wiktionary page resulting in one or many (flash) "Card"
+        objects containing the definitions for each word and its
+        corresponding part-of-speech.
+        """
         parsed_page = wtp.parse(self.text)
-        entries = []
 
         # Only use contents of target language
+        subsections = CardDataGenerator._extract_target_lang_sections(parsed_page, self.target_language)
+
+        # Relevant subsections containing translations
+        relevant_sections = CardDataGenerator._filter_pos_sections(subsections)
+
+        # Extract & format definitions
+        cards = CardDataGenerator._generate_cards(self.title, relevant_sections)
+
+        return cards
+
+    @staticmethod
+    def _extract_target_lang_sections(parsed_page: wtp.WikiText, target_language: str) -> List[wtp.Section]:
+        """
+        Extract only the main section in the specified target language and
+        retrieve subsections potentially containing words alongside their
+        definitions.
+        """
         target_lang_section = [
             section.contents
             for section in parsed_page.sections
-            if section.title == self.target_language
+            if section.title == target_language
         ][0]
-        subsections = wtp.parse(target_lang_section).get_sections(include_subsections=False, level=3) \
-                    + wtp.parse(target_lang_section).get_sections(include_subsections=False, level=4)
 
+        return wtp.parse(target_lang_section).get_sections(include_subsections=False, level=3) \
+                + wtp.parse(target_lang_section).get_sections(include_subsections=False, level=4)
 
-        # Relevant subsections containing translations
+    @staticmethod
+    def _filter_pos_sections(subsections: List[wtp.Section]) -> List[wtp.Section]:
+        """
+        Filter out sections not containing relevant definitions. Relevant
+        sections are distinguished by having names corresponding to
+        parts-of-speech.
+        """
+        RELEVANT_SECTIONS = tuple(['Adjective', 'Adverb', 'Conjunction', 'Determiner', 'Interjection', 'Noun',
+            'Number', 'Numeral', 'Ordinal number', 'Particle', 'Postposition', 'Preposition', 'Pronoun', 'Verb'])
+
         relevant_sections = [
             section
             for section in subsections
             if section.title is not None and section.title.startswith(RELEVANT_SECTIONS)
         ]
 
-        # Extract & format definition
+        return relevant_sections
+
+    @staticmethod
+    def _generate_cards(src_word: str, subsections: List[wtp.Section]) -> List[Card]:
+        """
+        Extract relevant definitions from a part-of-speech section and format
+        them appropriately for further processing.
+        """
         p = re.compile(r"^.*\$\$[^\{\}]*\$\$.*$")
-        for pos_section in relevant_sections:
+        entries = []
+
+        for pos_section in subsections:
+            # Retrieve all definitions from the section
             items = [ wtp.WikiText(def_item) for def_list in pos_section.get_lists() for def_item in def_list.items ]
+
+            # Format definition as plain text
             formatted_items = [ item.plain_text(replace_templates=False).strip() for item in items ]
+
+            # Evaluate template expressions
             processed_items = [ tp.process_templates(formatted_item) for formatted_item in formatted_items ]
+
+            # Clean up unwanted definitions
             cleaned_items = [ processed_item.strip() for processed_item in processed_items if not p.match(processed_item)]
-            entries.append(Card(self.title, pos_section.title, cleaned_items))
+
+            # Generate Card
+            entries.append(Card(src_word, pos_section.title, cleaned_items))
 
         return entries
 
