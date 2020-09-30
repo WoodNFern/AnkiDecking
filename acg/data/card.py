5db2d300461954ad6d2600000c0ad065bbf68171
Matthias Hermann <matthias.herm@mailbox.org>
2020-09-30 11:37:42 +0200

    Create Card objects from (parsed) JSON objects

--- acg/data/card.py
+++ acg/data/card.py
@@ -1,11 +1,21 @@
 import json
 
+from typing import List
+
 class Card():
 
-    def __init__(self, word: str, pos: str, definitions: list):
+    def __init__(self, word: str, pos: str, definitions: List[str]):
         self.word = word
         self.pos = pos
         self.definitions = definitions
+
+    @staticmethod
+    def from_json(json_obj: dict):
+        word = json_obj['word']
+        pos = json_obj['pos']
+        definitions = json_obj['definitions']
+
+        return Card(word, pos, definitions)
 
     def __repr__(self):
         return json.dumps(self.__dict__, separators=(',', ': '), indent=4)
